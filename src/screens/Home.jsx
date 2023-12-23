bbb9de8a72bd8332b965c93a1c35a391d0c76a10
Benjamin Ornodrff <btorndorff@gmail.com>
2023-12-23 02:04:58 -0500

    home

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -1,14 +1,31 @@
 // Home.jsx
 
 import React from 'react';
+import "../styles/styles.css"
 
 const Home = () => {
   return (
     <div className='container'>
-      <h1>Hello, World!</h1>
-      <p>This is a basic React component for the home screen.</p>
+      <div className="info-bar">
+        <p>Welcome, to this page dedicated to the Orndorff family history from our origins in Seagan, Germany, our emigration to the United States, and our modern whereabouts!</p>
+        <div className="info-button">Learn More</div>
+      </div>
+      <div className="highlight-1">
+        <div className="highlight-info">
+          <h1>AN ORNDORFF FAMILY<br /></h1>
+          <h3>A comprehensive book on the Orndorff family history</h3>
+        </div>
+      </div>
+      <div className="highlight-2">
+        <div className="highlight-info">
+          <h1>IRENE ORNDORFF<br /></h1>
+          <h3>the woman who worked under rosevelt</h3>
+        </div>
+      </div>
     </div >
   );
 };
 
 export default Home;
+
+// TODO: setup as redirects on click, stop text from moving with animation, make height of highlight same as screen height
